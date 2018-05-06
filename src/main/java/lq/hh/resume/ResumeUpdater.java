4a30ab8b4e0d806e7be34d2eac235f2657849ec8
slyisarenko-ilya <slyisarenko-ilya@mail.ru>
2018-05-06 18:24:58 +0300

    Лишний импорт

--- src/main/java/lq/hh/resume/ResumeUpdater.java
+++ src/main/java/lq/hh/resume/ResumeUpdater.java
@@ -32,7 +32,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import junit.framework.Assert;
 import lq.hh.exception.CannotUpdateException;
 import lq.hh.exception.NoPropertiesException;
 //
